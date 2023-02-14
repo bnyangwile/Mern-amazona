957f9f9e2dd38eceb78eb4bb274789367024a967
Benito <bnyangwile@gamil.com>
2023-02-14 18:43:29 +0100

    Video-07-Create-React-App

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim Shirt",
+      slug: "nike-slim-shirt", //what you see on the URL
+      category: "Shirts",
+      image: "/images/p1.jpg", // Width=679 x Heigth=829 pixels
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Adidas Fit Shirt",
+      slug: "adidas-fit-shirt", //what you see on the URL
+      category: "Shirts",
+      image: "/images/p2.jpg", //Width=679 x Heigth=829 pixels
+      price: 250,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      name: "Nike Slim Pant",
+      slug: "nike-slim-pant", //what you see on the URL
+      category: "Pants",
+      image: "/images/p3.jpg", // Width=679 x Heigth=829 pixels
+      price: 250,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      name: "Adidas Fit Pant",
+      slug: "adidas-fit-pant", //what you see on the URL
+      category: "Pants",
+      image: "/images/p4.jpg", // width=679 x Heigth=829 pixels
+      price: 65,
+      countInStock: 5,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+  ],
+};
+export default data;
